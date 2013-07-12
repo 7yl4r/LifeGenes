d9cad12d45c4575662aaf179a46ba6fe77b5c7b7
7yl4r <murray.tylar@gmail.com>
2013-07-12 18:30:13 -0400

    added cell movement and demo

--- golly/Scripts/Python/LifeGenes/lifegenes/cell.py
+++ golly/Scripts/Python/LifeGenes/lifegenes/cell.py
@@ -2,9 +2,58 @@
 
 from random import randrange
 
+import logging
+
 BASES = ['A','B','C','D']	#DNA Base Pairs (BP)
 DNA_MINLEN = 10			#min base pairs in a genome 
 DNA_MAXLEN = 100		#max base pairs in a genome
+
+#strings which decrease value in neural network connection
+ANN_DN_CODONS = [['ABDD', 'CAAB', 'ABAD', 'CBBC'],\
+['DDDA', 'DBAB', 'AADD', 'ABAB'],\
+['CDDC', 'DCDD', 'ACBD', 'BADC'],\
+['CBBB', 'BBDA', 'AACD', 'DDCA'],\
+['ADDA', 'CBDD', 'DBAA', 'DBAC'],\
+['CBAB', 'ACAD', 'ABCD', 'CBCB'],\
+['BBAC', 'DCCD', 'BCAB', 'BACC'],\
+['DDCC', 'BDCC', 'DADA', 'BADA'],\
+['DDDD', 'BBAB', 'BDDA', 'CDAD'],\
+['DDBD', 'BDBA', 'DCDA', 'BBAA'],\
+['BDAA', 'DBBC', 'BADD', 'BBDD'],\
+['ABAA', 'DDBC', 'ADBB', 'CCDA'],\
+['BCDC', 'CACA', 'CAAC', 'CBAD'],\
+['ADDC', 'DBDB', 'CCAA', 'CCBD'],\
+['BACD', 'AABC', 'DACD', 'BCBB'],\
+['ABDA', 'ACCC', 'BBDB', 'DDCD'],\
+['ADBD', 'CDDB', 'DDBA', 'DCBB'],\
+['CABC', 'CCDD', 'ACCD', 'ACBC'],\
+['CCCB', 'CACB', 'ACCA', 'DDCB']]
+
+#strings which increase value in neural network connection
+ANN_UP_CODONS = [['CDCB', 'BABB', 'ACAC', 'ABCB'],\
+['CCDB', 'CDAB', 'DDDB', 'DACC'],\
+['BBCC', 'ACDB', 'CADB', 'BDCB'],\
+['DBBA', 'CBAA', 'AACC', 'BCBA'],\
+['ACDA', 'BAAB', 'DADD', 'DDAD'],\
+['BDCD', 'AACA', 'AAAC', 'BBCB'],\
+['CAAA', 'DBDD', 'BCAC', 'AAAB'],\
+['DCDC', 'BCCD', 'DACA', 'BDBD'],\
+['CCBA', 'ADBC', 'BBCA', 'CBCC'],\
+['BCDD', 'BACB', 'DAAC', 'DCBA'],\
+['CCDC', 'DADB', 'CACC', 'BDDB'],\
+['BDCA', 'ADAB', 'BDAB', 'BBBB'],\
+['DCDB', 'DACB', 'ADCA', 'AABB'],\
+['DCAC', 'ABAC', 'ABBC', 'BDAC'],\
+['DABA', 'ACBA', 'BCCA', 'CBCD'],\
+['CCAD', 'CABB', 'DDAA', 'ABBB'],\
+['CBDB', 'BABC', 'BCCB', 'ADDB'],\
+['ADAC', 'BBBD', 'DBBD', 'DDAB'],\
+['DAAB', 'ADBA', 'AAAD', 'CBBA']]
+
+DARK_CODON = 'BD'
+LIGHT_CODON = 'AB'
+
+# TODO: genetically calculated values need only be calculated once and then stored for future reference. This will greatly improve efficiency.
 
 class cell:
 	def __init__(self,X,Y):	#TODO: add DNA= at end
@@ -14,20 +63,13 @@ class cell:
 
 	# return color value 1-255 generated from first 10 BP of DNA
 	def getColor(self):	
-		color = 128	#starting color in middle of range
-		maxColor = 200
+		color = 110	#starting color in middle of range
+		maxColor = 220
 		minColor = 1
-		dc    = 15 	#delta color; amount of change when codon is detected
+		dc    = 5 	#delta color; amount of change when codon is detected
 		lightCodon = 'AB' #string which decreases color value
 		darkCodon  = 'BD' #string which increases color value
-		for i in range(len(self.DNA)-1):
-			if (str(self.DNA[i])+str(self.DNA[i+1])) == darkCodon:
-				color+=dc
-			if (str(self.DNA[i])+str(self.DNA[i+1])) == lightCodon:
-				color-=dc
-		if color > maxColor : color = maxColor
-		if color < minColor : color = minColor
-		return color
+		return self.getGeneticValue(color,maxColor,minColor,dc,DARK_CODON,LIGHT_CODON)
 
 	# generates a random DNA string using bases defined in BASES
 	def randomizeDNA(self):
@@ -56,10 +98,125 @@ class cell:
 			cellDNA.append(genes[inheritFrom][i])
 		self.DNA = cellDNA
 
-	#TODO: allows the cell to move in directions up,right,down,left (0,1,2,3)
-#	def move(self,direction,distance=1):
-		# set state=0 of old location in golly
-		# change cell object attributes
-		# set state=1 of new location in golly
+	# get visual information about the cells nearby
+	def getVisualInput(self,stateGetter):
+		#setup visual input 
+		return [stateGetter(self.x-1,self.y-2),\
+		        stateGetter(self.x  ,self.y-2),\
+		        stateGetter(self.x+1,self.y-2),\
+		        stateGetter(self.x-2,self.y-1),\
+		        stateGetter(self.x-1,self.y-1),\
+		        stateGetter(self.x  ,self.y-1),\
+		        stateGetter(self.x+1,self.y-1),\
+		        stateGetter(self.x+2,self.y-1),\
+		        stateGetter(self.x-2,self.y  ),\
+		        stateGetter(self.x-1,self.y  ),\
+		        stateGetter(self.x+1,self.y  ),\
+		        stateGetter(self.x+2,self.y  ),\
+		        stateGetter(self.x-2,self.y+1),\
+		        stateGetter(self.x-1,self.y+1),\
+		        stateGetter(self.x  ,self.y+1),\
+		        stateGetter(self.x+1,self.y+1),\
+		        stateGetter(self.x+2,self.y+1),\
+		        stateGetter(self.x-1,self.y+2),\
+		        stateGetter(self.x  ,self.y+2),\
+		        stateGetter(self.x+1,self.y+2)]
+
+	# returns a numeric value determined from DNA; codons MUST be same length TODO: fix this
+	def getGeneticValue(self,startVal,maxVal,minVal,delta,upCodon,downCodon):
+		genes = ''.join(self.DNA)	#convert from DNA char array to string (to use slicing)
+		v = startVal
+		pass # logging.debug('looking for strings '+str(downCodon)+' and '+str(upCodon))
+		L = len(downCodon) # == len(upCodon)
+		for i in range( len(self.DNA)-L ):
+			s_dn = genes[i:(i+len(downCodon))]
+			s_up = genes[i:(i+len(upCodon))]
+			pass # logging.debug('checking strings '+str(s_dn)+' and '+str(s_up))
+			if s_dn == downCodon:
+				v-=delta
+			if s_up == upCodon:
+				v+=delta
+		if v > maxVal : v = maxVal
+		if v < minVal : v = minVal
+		return v
+
+	# gets the weights for ANN connections (see wiki for more info)
+	def getNNweights(self):
+		startV = 0	#starting color in middle of range
+		maxV = 1000
+		minV = -1000
+		delt = 1 	# amount of change when codon is detected
+		num_outs = 4
+		num_ins  = len(ANN_UP_CODONS)/4
+		weights = [ [startV]*num_outs ]*num_ins	# inputs x outputs list
+		logging.debug(str(num_ins)+'x'+str(num_outs)+' weights list created:\n'+str(weights))
+		for i in range(num_ins):
+			for o in range(num_outs):
+				weights[i][o] = self.getGeneticValue(startV,maxV,minV,delt,ANN_UP_CODONS[i][o],ANN_DN_CODONS[i][o])
+		return weights
+
+	# moves the cell as determined by DNA-encoded neural network (see wiki for more info)
+	def move(self,stateSetter,stateGetter):
+		num_ins  = len(ANN_UP_CODONS)/4
+		v = self.getVisualInput(stateGetter)
+		w = self.getNNweights()
+		output = [0]*4
+		for o in range(4):
+			for i in range(num_ins):
+				output[o] = v[i]*w[i][o]
+		direction = max(output) #cell moves in direction of greatest output
+		if direction < 1: #simple threshold
+			return
+		else:
+			if direction == 0:
+				d = 'up'
+			elif direction ==1:
+				d = 'right'
+			elif direction == 2:
+				d = 'down'
+			elif direction == 3:
+				d = 'left'
+			else: 
+				logging.error('direction number "'+str(direction)+'" not recognized')
+				return
+			self.moveOne(d,stateSetter,stateGetter)
+		
+	# allows the cell to move one space in given direction
+	def moveOne(self,direction,stateSetter,stateGetter):
+		# find new cell object attributes
+		if direction == 'up':
+			newX = self.x
+			newY = self.y - 1
+		elif direction == 'down':
+			newX = self.x
+			newY = self.y + 1
+		elif direction == 'left':
+			newX = self.x - 1
+			newY = self.y
+		elif direction == 'right':
+			newX = self.x + 1
+			newY = self.y
+		else:
+			logging.warn('direction "'+str(direction)+'" not recognized by cell')
+			return
+		#check for occupied space
+		if stateGetter(newX,newY) == 1:	# another cell is already there	
+			pass # TODO: try to push cell?
+		else: # space is clear; move into it
+			stateSetter(self.x,self.y,0)	# set state=0 of old location in golly
+			self.x = newX
+			self.y = newY
+			stateSetter(self.x,self.y,1)	# set state=1 of new location in golly
+
+	# allows the cell to move in directions up,right,down,left a given distance
+	def moveNum(self,direction,distance,stateSetter,stateGetter):
+		if distance == 0:
+			logging.debug('cell movement distance == 0')
+			return
+		for x in range(distance):
+			self.moveOne(direction,stateSetter,stateGetter)
+
+
+
 
 
---
+++ golly/Scripts/Python/LifeGenes/movingCells.py
@@ -0,0 +1,185 @@
+# this script creates a cell genome for each cell in the current layer, 
+# then displays the genetic 'color' of each cell.
+# The script then begins cycling between rounds of cell movement and cell evolution.
+# Please see the [LifeGenes project for genetic cellular automaton](https://github.com/7yl4r/LifeGenes) for more info.
+
+from glife import rect
+import golly as g
+
+from LifeGenes.lifegenes.cellList import cellList	#import LifeGenes cell definition
+from LifeGenes.lifegenes.cell     import cell as LGcell
+
+# import time	#for delays when debugging
+
+#BEGIN setup log file for this script:
+from os.path import expanduser
+home = expanduser("~")
+logDir = home+'/LifeGenes/__logs'
+from os import mkdir
+try:
+	mkdir(logDir)
+except OSError:
+	pass # probably the dir already exists...
+logName = logDir+'/movingCells.log'
+import logging
+print str(logging.getLogger())
+logging.basicConfig(filename=logName,\
+	                level=logging.DEBUG,\
+	                format='%(asctime)s %(levelname)s:%(message)s',\
+                   filemode='w')
+#NOTE: this file only resets when golly is restarted, 
+#      otherwise the log object is retained and reused, 
+#      appending to the file as the script is run multiple times
+g.show('created .log at '+str(logName))
+#END log file setup
+
+class run():
+	def __init__(self):
+		logging.info('script started')
+		self.startUp()
+		self.drawColor()
+		logging.info('setup complete; beginning evolution cycles')
+		try:
+			while(True):	#until stopped by golly
+				for i in range(3): # 5 rounds of cell movement				
+					self.cellMotions()
+					self.drawColor()
+					g.update()
+					#time.sleep(1)
+
+				# one round of evolution:
+				g.step()
+				#g.update()
+				self.update()
+				self.drawColor()
+				g.update()
+				#time.sleep(1)
+		finally:
+				logging.info('cycling halted from external source (probably golly)')
+				#TODO: save cell genomes to file
+
+	# cellMotions allows cells to move around in their environment while no evolution takes place
+	#     motion is based on values calculated from the cell's neural network
+	#     layer must be on the original layer, NOT the color layer.
+	def cellMotions(self):
+		for cell in self.cellList.cells:
+			direc = 'right' #cell.getMovementDirection()	
+			dist  = 1 #cell.getSpeed()
+			cell.move(g.setcell,g.getcell)
+			#cell.moveNum(direc,dist,g.setcell,g.getcell)
+
+	#removes genetic material from cell no longer in environment layer
+	def removeDeadCells(self):
+		originL = g.getlayer() #starting layer
+		newPattern = g.getcells(g.getrect())
+		#g.note('newPattern='+str(newPattern))
+		g.setlayer(self.colorIndex) #switch to color layer
+		cellsToKill=list() #must keep this list to do at end or loops get fudged
+		for oldCell in self.cellList.cells:
+			for newCellIndex in range(len(newPattern)/2):
+				xi = newPattern[newCellIndex*2] #newCell location
+				yi = newPattern[newCellIndex*2+1]
+				if oldCell.x==xi and oldCell.y==yi:
+					#g.note('cell at '+str(xi)+','+str(yi)+' found.')
+					break #cell found in new pattern; move on
+			else: #if cell not found in new pattern; cell has died.
+				#g.note('cell at '+str(oldCell.x)+','+str(oldCell.y)+' not found.')
+				g.setcell(oldCell.x,oldCell.y,0)
+				cellsToKill.append([oldCell.x,oldCell.y])
+		for c in cellsToKill:
+			self.cellList.killCellAt(c[0],c[1])
+		g.setlayer(originL)
+
+	#uses g.update() and fixes self.cellList to match
+	def update(self):
+		#g.autoupdate(True)#while debugging
+
+		newPattern = g.getcells(g.getrect())
+		# generate DNA for new cells
+		cellsToAppend = list() #save this & add at end to not muck up the list
+		for newCellIndex in range(len(newPattern)/2): #for all cells in newCellIndex
+			#add the cell if it does not already exist
+			xi = newPattern[newCellIndex*2] #newCell location
+			yi = newPattern[newCellIndex*2+1]
+			if self.cellList.findCell(xi,yi)==None:
+				parents = list()
+				newCell = LGcell(xi,yi)
+				neighbors = [[newCell.x-1,newCell.y+1],\
+					     [newCell.x  ,newCell.y+1],\
+					     [newCell.x+1,newCell.y+1],\
+					     [newCell.x+1,newCell.y  ],\
+					     [newCell.x+1,newCell.y-1],\
+					     [newCell.x  ,newCell.y-1],\
+					     [newCell.x-1,newCell.y-1],\
+					     [newCell.x-1,newCell.y  ]]
+				for n in neighbors:
+					if self.cellList.findCell(n[0],n[1])!=None: #add neighbor as parent if exists
+						parents.append(self.cellList.findCell(n[0],n[1]))
+					#if len(parents)==3: #this *might* improve efficiency, but only a little
+					#	break
+				#g.note(str(newCell.x)+','+str(newCell.y)+' parents are: '+str(parents))
+				newCell.inheritDNA(parents)
+				cellsToAppend.append(newCell)
+			#else: keep old cell info; do nothing
+		for c in cellsToAppend:
+			self.cellList.cells.append(c)
+
+		# remove dead cells
+		self.removeDeadCells()
+
+	# prepares the needed data structures
+	def startUp(self):
+		originL = g.getlayer() #starting layer
+		if g.empty(): g.exit("The pattern is empty.")
+		# check for existing geneticColor layer
+		originL = g.getlayer() #starting layer
+		currindex = 0
+		self.colorIndex = -1
+		while currindex < g.numlayers():
+			if g.getname(currindex) == 'geneticColor':
+				# continue from where we left off
+				self.colorIndex = currindex
+				g.show('geneticColor layer already exists!')
+  				 #TODO: read cell genes from a file and make self.cellList
+				break
+			else: currindex+=1
+		if self.colorIndex == -1:	#if we didn't find existing colorLayer
+			#set up new colorLayer
+			startpatt = g.getcells(g.getrect()) # get starting pattern array [x1,y1,x2,y2,...]
+	
+			self.cellList = cellList(startpatt)
+
+			logging.info(str(len(startpatt)/2)+' cells found, '+str(len(self.cellList.cells))+' trait objects created.')
+
+
+			# add color layer
+			if g.numlayers() + 1 > g.maxlayers():
+				g.exit("You need to delete a layer.")
+
+			self.colorIndex = g.addlayer()     # create layer for colors
+			g.setname('geneticColor')
+			g.setrule('constant')	#use custom constant rule
+			g.setcolors([0,255,0, 0,0,255]) #live states vary from green to blue
+		g.setlayer(originL) # set layer back to original layer
+
+	
+	#draws colors onto custom color layer
+	def drawColor(self):
+		originL = g.getlayer() #starting layer
+		g.setlayer(self.colorIndex)
+	
+		# clear the layer
+		try:
+			g.select(g.getrect())
+			g.clear(0)  
+		except: 		
+			logging.warn('cannot clear empty layer')
+		g.select([])
+
+		# show the colors of each cell
+		for c in self.cellList.cells:
+			g.setcell(c.x,c.y,c.getColor())
+		g.setlayer(originL) # set layer back to original layer
+
+#MAIN
+run()
