f2d215a7da0cd375bfe1eb5983f7a96d31f0617e
Tylar Murray <murray.tylar@gmail.com>
2014-11-30 17:39:56 -0500

    building blocks added

---
+++ startup.sh
@@ -0,0 +1,9 @@
+echo 'compiling dust templates...'
+./node_modules/.bin/duster templates templates/templates.js
+
+echo 'compiling less css...'
+#./node_modules/.bin/lessc -x ./less/agency/agency.less > ./css/agency.min.css
+#./node_modules/.bin/lessc -x ./less/AdminLTE.less > ./css/AdminLTE.css
+
+echo 'starting server for testing...'
+python -m SimpleHTTPServer
