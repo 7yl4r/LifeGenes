dfec52a3dce221315d75bba3de0d1b7b252337f9
Tylar Murray <murray.tylar@gmail.com>
2014-11-30 16:45:59 -0500

    burn it all! mwahahahaha!

--- copyToGolly.sh
+++
@@ -1,6 +0,0 @@
-PATH_TO_GOLLY="/usr/share/"
-
-sudo cp -r ./golly/Patterns $PATH_TO_GOLLY"golly/"
-sudo cp -r ./golly/Rules $PATH_TO_GOLLY"golly/"
-
-sudo cp --parents `find ./golly/Scripts/Python -name \*.py` $PATH_TO_GOLLY
