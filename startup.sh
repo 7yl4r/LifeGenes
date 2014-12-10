0fdf920d1f1f487ebe65e0e463bb25c8f00bee62
7yl4r <murray.tylar@gmail.com>
2014-12-10 12:06:18 -0500

    now using custom colors in app css

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@ echo 'compiling dust templates...'
 
 echo 'compiling less css...'
 ./node_modules/.bin/lessc -x ./less/bootstrap/bootstrap.less > ./css/bootstrap.min.css
+./node_modules/.bin/lessc -x ./less/cellStates.less > ./css/cellStates.min.css
+./node_modules/.bin/lessc -x ./less/proteins.less > ./css/proteins.min.css
 
 echo 'transpiling .coffee into .js...'
 ./node_modules/.bin/coffee --output js/coffee --compile coffee
