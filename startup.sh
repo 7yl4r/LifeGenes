d907c534a5c04a2ef3a25bd8dde51e3a4d061b06
7yl4r <murray.tylar@gmail.com>
2014-12-10 11:43:18 -0500

    now building custom bootstrap

--- startup.sh
+++ startup.sh
@@ -2,8 +2,7 @@ echo 'compiling dust templates...'
 ./node_modules/.bin/duster templates templates/templates.js
 
 echo 'compiling less css...'
-#./node_modules/.bin/lessc -x ./less/agency/agency.less > ./css/agency.min.css
-#./node_modules/.bin/lessc -x ./less/AdminLTE.less > ./css/AdminLTE.css
+./node_modules/.bin/lessc -x ./less/bootstrap.less > ./css/bootstrap.min.css
 
 echo 'transpiling .coffee into .js...'
 ./node_modules/.bin/coffee --output js/coffee --compile coffee
